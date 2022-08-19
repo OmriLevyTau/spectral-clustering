a1a91edf04003cbc4fd1fa98fd618af48800aa3b
idanisraeli1 <idanisraeli@mail.tau.ac.il>
2022-08-19 17:49:33 +0300

    all methods implementation

--- spkmeans.c
+++ spkmeans.c
@@ -14,13 +14,13 @@ double* extract_diagonal(int n, double** A);
 int find_eigengap(int n, double** A);
 int compare_int_reversed_order(const void *p1, const void *p2);
 int* find_ij (int n, double** A);
-double** create_P (int n, int i, int j, double** A, double** P_prev);
+void update_P (int n, int i, int j, double** A, double** P_prev);
 double*** create_jacobi_matrix (int n, double** L_norm);
 double** jacobi_algorithm (int k, int n, double** L_norm);
-double** create_T(int n, double** U);
+double** create_T(int rows, int cols, double** U);
 void print_error_and_exit();
 double** buildMatrix(int rows, int cols);
-double sum_array(int n, double* arr);
+double sum_array(int n, const double* arr);
 double** matrix_mult(int n, double **A, double **B);
 int free_matrix( int rows, double** pointer);
 double compute_off_diag(int n, const double **A);
@@ -28,16 +28,36 @@ double compute_theta(double A_ij, double A_ii, double A_jj);
 double compute_t(double theta);
 double compute_c(double t);
 void reset_matrix(int n, double**A);
+double** create_copy(int n, double** A);
 void update_A_to_A_tag(int n, int i, int j, double**A);
 int* find_k_max_indices(int n, int k, double** A);
 double** create_U (int n, int k, int* indices, double** V);
+double sum_squared_array(int k, const double* arr);
 double** read_data(int rows, int cols, char* filePath);
 int count_cols(char* filePath);
 int count_rows(char* filePath);
 
-/*
- * L2 Norm distance of the vector A - B
- * */
+
+
+/* build new matrix */
+double** buildMatrix(int rows, int cols){
+    /* Creates empty matrix of size rows x cols */
+    int i;
+    double **a = calloc(rows, sizeof(int*));
+    if (a==NULL){
+        print_error_and_exit();
+    }
+    for (i=0;i<rows;i++){
+        a[i] = calloc(cols, sizeof(double));
+        if (a[i]==NULL){
+            print_error_and_exit();
+        }
+    }
+    return a;
+}
+
+
+/* L2 Norm distance of the vector A - B */
 double l2_norm_dist(int n, const double *A, const double *B){
     int i;
     double sum = 0;
@@ -47,9 +67,7 @@ double l2_norm_dist(int n, const double *A, const double *B){
     return sqrt(sum);
 }
 
-/*
- * calculates the weighted distance between points A and B
- * */
+/* calculates the weighted distance between points A and B */
 double weighted_distance(int n, const double *A, const double *B){
     double diff = l2_norm_dist(n,A,B);
     return exp(-0.5 * diff);
@@ -77,9 +95,7 @@ double** create_wam(int n, double** X){
     return W;
 }
 
-/*
- * creates identity matrix
- * */
+/* creates identity matrix */
 double** create_identity_matrix(int n){
     int i;
     double** eye = buildMatrix(n,n);
@@ -89,10 +105,7 @@ double** create_identity_matrix(int n){
     return eye;
 }
 
-/*
- * creates Diagonal Degree Matrix
- * uses sum_array
- * */
+/* creates Diagonal Degree Matrix, uses sum_array */
 double** create_ddg(int n, double** X){
     int i;
     double** W = create_wam(n,X);
@@ -104,9 +117,7 @@ double** create_ddg(int n, double** X){
     return D;
 }
 
-/*
- * creates D^(-0.5)
- * */
+/* creates D^(-0.5) */
 double** create_ddg_inverse(int n, double** X){
     int i;
     double** D = create_ddg(n, X);
@@ -118,9 +129,7 @@ double** create_ddg_inverse(int n, double** X){
     return D_inverse;
 }
 
-/*
- * creates Normalized Graph Laplacian
- * */
+/* creates Normalized Graph Laplacian */
 double** create_Lnorm(int n, double** X){
     int i;
     double** D = create_ddg(n,X);
@@ -138,6 +147,7 @@ double** create_Lnorm(int n, double** X){
     return L_norm;
 }
 
+/* gets matrix with eigenvalues on its diagonal, and returns an array with those eigenvalues*/
 double* extract_diagonal(int n, double** A){
     int i;
     double* eigenvalues = calloc(n, sizeof(double));
@@ -148,9 +158,7 @@ double* extract_diagonal(int n, double** A){
 }
 
 
-/*
- * finds eigengap heuristic
- * */
+/* finds eigengap heuristic */
 int find_eigengap(int n, double** A){
     int i;
     int k = 0;
@@ -168,7 +176,7 @@ int find_eigengap(int n, double** A){
     return k;
 }
 
-
+/* helper for finding eigengap */
 int compare_int_reversed_order(const void *p1, const void *p2){
     /* returns positive if a<b, 0 if a==b, negative if a>b*/
     const int *q1 = p1, *q2 = p2;
@@ -176,10 +184,7 @@ int compare_int_reversed_order(const void *p1, const void *p2){
 }
 
 
-/*
- * finds the indices i,j
- * under assumption that K>1
- * */
+/* finds the indices i,j , under assumption that K>1 */
 int* find_ij (int n, double** A){
     int a, b;
     int* indices = calloc(2, sizeof(int));
@@ -202,12 +207,12 @@ int* find_ij (int n, double** A){
 
 
 /*
- * uses find_ij and create P_m
+ * update P for every iteration
 1. compute t, s, c, theta (calls helpers)
 2. reset temp (I) (reset_matrix)
 3. update P_prev and return it
  * */
-double** create_P (int n, int i, int j, double** A, double** P_prev){
+void update_P (int n, int i, int j, double** A, double** P_prev){
     double theta = compute_theta(A[i][j], A[i][i], A[j][j]);
     double t = compute_t(theta);
     double c = compute_c(t);
@@ -217,59 +222,94 @@ double** create_P (int n, int i, int j, double** A, double** P_prev){
     P_prev[j][j] = c;
     P_prev[i][j] = s;
     P_prev[j][i] = s * -1;
-    return P_prev;
 }
 
 
-/*
- * creates V (eigenvectors as columns) and A' (diagonal, eigenvalues of original A) 
- * */
- double*** create_jacobi_matrix (int n, double** L_norm){
-     return NULL;
- }
- /*
- * 
+/* creates V (eigenvectors as columns) and A' (diagonal, eigenvalues of original A)
+
  V = I (create_identity);
- A = copy of L_norm
+ A = return copy of L_norm
  initialize temp(create_identity);
  for(int m=0; m<100; m++){
     1. compute i,j
-    2. P_m = create_P (updated P_zona)
+    2. P_m = update_P (updated P_zona)
     3. compute off(A)
     4. set A=A' (call update_A_to_A_tag)
     6. V =V*P_m (matrix_mult)
     7. if(off(A)^2-off(A')^2<=eps){ break;}
  }
-now V holds eigenvectors and A holds eigenvalues
-free(temp);
-return V and A
- */
+ now V holds eigenvectors and A holds eigenvalues
+ free(temp);
+ return V and A
+ * */
+ double*** create_jacobi_matrix (int n, double** L_norm){
+     double*** VandA = calloc(2, sizeof(double**));
+     double** V = create_identity_matrix(n);
+     double** A = create_copy(n,L_norm);
+     double** P_m = create_identity_matrix(n);
+     double** temp;
+     double epsilon = 1.0/100000;
+     int m, i, j;
+     double off_A, off_A_tag;
+     int* arr_ij;
+     for(m=0; m<100; m++){
+         arr_ij = find_ij(n, A);
+         i = arr_ij[0];
+         j = arr_ij[1];
+         update_P(n, i, j, A, P_m);
+         off_A = compute_off_diag(n,A);
+         update_A_to_A_tag(n,i,j,A); /*now A is A'*/
+         off_A_tag = compute_off_diag(n,A);
+         temp = V;
+         V = matrix_mult(n, V, P_m);
+         free_matrix(n,temp);
+         if(((off_A*off_A)-(off_A_tag*off_A_tag))<=epsilon){ break;}
+     }
+     VandA[0] = V;
+     VandA[1] = A;
+     free_matrix(n,P_m);
+     free_matrix(n,temp);
+     return VandA;
+ }
 
 
 /*
  * run jacobi algorithm
- * */
-double** jacobi_algorithm (int k, int n, double** L_norm){
-    return L_norm;
-
-    /* complete */
-}
-/*
  * 1. call create_jacobi_matrix and compute V and A
  * 2. if k=0, calls find_eigengap and compute that required k
- * 3. indices= find_k_max_indices 
+ * 3. indices= find_k_max_indices
  * 4. create and return U (call create_U)
  * */
+double** jacobi_algorithm (int k, int n, double** L_norm){
+    int* indices;
+    double** U;
+    double*** VandA = create_jacobi_matrix(n,L_norm);
+    double** V = VandA[0];
+    double** A = VandA[1];
+    if(k==0){
+        k = find_eigengap(n,A);
+    }
+    indices = find_k_max_indices(n,k,A);
+    U = create_U(n,k,indices,V);
+    free_matrix(n,V);
+    free_matrix(n,A);
+    free(VandA);
+    return U;
+}
 
 
-/*
- * normalizes U and returns it
- * */
-double** create_T(int n, double** U){
-    double** T = buildMatrix(n,n);
+/* normalizes U and returns it */
+double** create_T(int rows, int cols, double** U){
+    int i,j;
+    double cur_row_sum;
+    double** T = buildMatrix(rows,cols);
+    for(i=0; i<rows; i++){
+        for(j=0;j<cols;j++){
+            cur_row_sum= sum_squared_array(cols,U[i]);
+            T[i][j] = U[i][j] / cur_row_sum;
+        }
+    }
     return T;
-
-    /* complete */
 }
 
 
@@ -283,33 +323,10 @@ void print_error_and_exit(){
     exit(1);
 }
 
-/*
-build new matrix 
-*/
-double** buildMatrix(int rows, int cols){
-    /*
-     * Creates empty matrix of size rows x cols
-     */
-    int i;
-    double **a = calloc(rows, sizeof(int*));
-    if (a==NULL){
-        print_error_and_exit();
-    }
-    for (i=0;i<rows;i++){
-        a[i] = calloc(cols, sizeof(double));
-        if (a[i]==NULL){
-            print_error_and_exit();
-        }
-    }
-    return a;
-}
 
 
-
-/*
- * sum of array
- * */
-double sum_array(int n, double* arr){
+/* sum of array */
+double sum_array(int n, const double* arr){
     int i;
     double sum = 0;
     for(i=0; i<n; i++){
@@ -318,9 +335,17 @@ double sum_array(int n, double* arr){
     return sum;
 }
 
-/*
- * Matrix multiplication
- * */
+/* returns sum of squared items of an array  */
+double sum_squared_array(int k, const double* arr){
+    int i;
+    double sum = 0;
+    for(i=0; i<k; i++){
+        sum += arr[i]*arr[i];
+    }
+    return sum;
+}
+
+/* Matrix multiplication */
 double** matrix_mult(int n, double **A, double **B){
     double sum;
     int a, b, c;
@@ -452,20 +477,50 @@ void update_A_to_A_tag(int n, int i, int j, double**A){
  * K times find the next maximum from eigenvalues in A, and returns those indices
  * */
 int* find_k_max_indices(int n, int k, double** A){
-    int* indices= calloc(k, sizeof(int));
-    return indices;
+    int* k_indices= calloc(k, sizeof(int));
+    double* all_eigenvals = extract_diagonal(n,A);
+    int i,j;
+    double max_eigenval;
+    int max_ind;
+    for(i=0;i<k;i++){
+        max_eigenval=all_eigenvals[0];
+        max_ind = 0;
+        for(j=0;j<n;j++){
+            if(all_eigenvals[j]>max_eigenval) {
+                max_eigenval = all_eigenvals[j];
+                max_ind = j;
+            }
+        }
+        k_indices[i] = max_ind;
+        all_eigenvals[max_ind] = INT_MIN;
+    }
+    free(all_eigenvals);
+    return k_indices;
 }
-/* complete */
-
 
 
 /*
- * creates U from V bu largest K eigenvalues
+ * creates U from V by largest K eigenvalues
  * */
 double** create_U (int n, int k, int* indices, double** V){
     double** U = buildMatrix(n,k);
+    int i, j, cur_ind;
+    for(i=0; i<k; i++){
+        cur_ind = indices[i];
+        for(j=0; j<n; j++){
+            U[j][i] = V[j][cur_ind];
+        }
+    }
     return U;
+}
 
-
-    /*complete*/
+double** create_copy(int n, double** A){
+    int i,j;
+    double** new_mat = buildMatrix(n,n);
+    for(i=0; i<n; i++){
+        for(j=0;j<n;j++){
+            new_mat[i][j] = A[i][j];
+        }
+    }
+    return new_mat;
 }
